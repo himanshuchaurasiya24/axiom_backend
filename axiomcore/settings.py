f895e6938a28b5cb9a8f5bc9ceeca2d9c56b2fc3
Himanshu Chaurasiya <himanshuchaurasiya24@gmail.com>
2025-11-10 19:26:47 +0530

    ..

--- axiomcore/settings.py
+++ axiomcore/settings.py
@@ -4,12 +4,14 @@ from datetime import timedelta
 BASE_DIR = Path(__file__).resolve().parent.parent
 
 SECRET_KEY = 'django-insecure-+@(awooy=f=qwd#@1(o_ou0$^i+s5*141#gi5$^emsgv$n8d!d'
-ALLOWED_HOSTS = ['192.168.31.76', 'localhost',
+ALLOWED_HOSTS = [
+    '192.168.1.44', 
+    'localhost',
  '127.0.0.1']
 CORS_ALLOWED_ORIGINS = [
     "http://localhost:8080",
     "http://127.0.0.1:8080",
-    "http://192.168.31.76:8000",
+    "http://192.168.1.44:8000",
 ]
 CORS_ALLOW_ALL_ORIGINS = True
 DEBUG=True

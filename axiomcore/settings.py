6239df47d203bae0fb3511ad98dbf1cb306028c2
Himanshu Chaurasiya <himanshuchaurasiya24@gmail.com>
2025-12-08 18:10:09 +0530

    fixed app info api error 404

--- axiomcore/settings.py
+++ axiomcore/settings.py
@@ -86,7 +86,7 @@ REST_FRAMEWORK = {
 }
 
 SIMPLE_JWT = {
-    "ACCESS_TOKEN_LIFETIME": timedelta(seconds=30),
+    "ACCESS_TOKEN_LIFETIME": timedelta(minutes=30),
     "REFRESH_TOKEN_LIFETIME": timedelta(days=7),
     "ROTATE_REFRESH_TOKENS": True,
 }
